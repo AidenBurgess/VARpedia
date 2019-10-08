f3d780db6b03e3f9d7e8ddfb5ab46b5ef8b0a8a5
Aiden Burgess <aiden9874@gmail.com>
2019-10-09 09:56:30 +1300

    Time slider added
    Can click anywhere on the timeline to jump there
    Can drag through the timeline
    Custom indicator when dragging timeline

--- src/app/VideoPlayerController.java
+++ src/app/VideoPlayerController.java
@@ -1,9 +1,10 @@
 package app;
 
 import java.io.File;
-
+import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXToggleButton;
-
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.media.Media;
@@ -13,17 +14,19 @@ import javafx.scene.media.MediaPlayer.Status;
 import javafx.util.Duration;
 
 public class VideoPlayerController {
-	
+
 	@FXML
 	private MediaView screen;
 	@FXML
 	private Label timeLabel;
 	@FXML
 	private JFXToggleButton toggleMusicButton;
-	
+	@FXML
+	private JFXSlider timeSlider;
+
 	private MediaPlayer player;
 	private MediaPlayer music;
-	
+
 	public void setSource(String source) {
 		// Setup video player with source file
 		File fileUrl = new File("videos/" + source + ".mp4"); 
@@ -31,58 +34,93 @@ public class VideoPlayerController {
 		player = new MediaPlayer(video);
 		player.setAutoPlay(true);
 		screen.setMediaPlayer(player);
-		
+		slider();
+
 		// Setup background music player
-	    fileUrl = new File("backgroundMusic" + ".wav");
-	    Media audio = new Media(fileUrl.toURI().toString());
-	    music = new MediaPlayer(audio);
-	    music.setAutoPlay(true);
-	    music.setMute(true);
-	    music.setCycleCount(MediaPlayer.INDEFINITE);
-		
-	    // Timer label tracks the time of the video
+		fileUrl = new File("backgroundMusic" + ".wav");
+		Media audio = new Media(fileUrl.toURI().toString());
+		music = new MediaPlayer(audio);
+		music.setAutoPlay(true);
+		music.setMute(true);
+		music.setCycleCount(MediaPlayer.INDEFINITE);
+
+		// Timer label tracks the time of the video
 		player.currentTimeProperty().addListener((observable,oldValue,newValue) -> {
-				String time = "";
-				time += String.format("%02d", (int)newValue.toMinutes());
-				time += ":";
-				time += String.format("%02d", (int)newValue.toSeconds()%60);
-				timeLabel.setText(time);
+			String time = "";
+			time += String.format("%02d", (int)newValue.toMinutes());
+			time += ":";
+			time += String.format("%02d", (int)newValue.toSeconds()%60);
+			timeLabel.setText(time);
 		});
 	}
-	
+
+	private void slider() {
+		// Providing functionality to time slider 
+		player.currentTimeProperty().addListener(ov -> updatesValues());
+
+		// Inorder to jump to the certain part of video 
+		timeSlider.valueProperty().addListener(ov-> { 
+			if (timeSlider.isPressed()) {
+				player.pause();
+				double percent = timeSlider.getValue()/player.getTotalDuration().toSeconds();
+				player.seek(player.getTotalDuration().multiply(percent)); 
+			}
+		}); 
+		timeSlider.setOnMouseReleased(e-> player.play());
+		timeSlider.setOnDragDropped(e-> player.play());
+
+		// Custom indicator labelling
+		timeSlider.setValueFactory(arg0-> {
+			return Bindings.createStringBinding(()->{
+				String time = "";
+				time += String.format("%02d", (int)player.getCurrentTime().toMinutes());
+				time += ":";
+				time += String.format("%02d", (int)player.getCurrentTime().toSeconds()%60);
+				return time;
+			}, timeSlider.valueProperty());
+		});
+	}
+
+	private void updatesValues() { 
+		Platform.runLater(()-> {
+			timeSlider.setMax(player.getTotalDuration().toSeconds());
+			timeSlider.setValue(player.getCurrentTime().toSeconds());
+		}); 
+	} 
+
 	@FXML
-    private void playPause() {
+	private void playPause() {
 		if (player.getStatus() == Status.PLAYING) {
 			player.pause();
 		} else {
 			player.play();
 		}
 	}
-	
+
 	@FXML
-    private void back() {
+	private void back() {
 		player.seek( player.getCurrentTime().add( Duration.seconds(-5)) );
 	}
-	
+
 	@FXML
-    private void forward() {
+	private void forward() {
 		player.seek( player.getCurrentTime().add( Duration.seconds(5)) );
 	}
-	
+
 	@FXML
 	private void mute() {
 		player.setMute( !player.isMute() );
 	}
-	
+
 	@FXML
 	private void toggleMusic() {
 		music.setMute(!toggleMusicButton.isSelected());
 	}
-	
+
 	@FXML
 	private void quit() {
-    	timeLabel.getScene().getWindow().hide();
-    	shutdown();
+		timeLabel.getScene().getWindow().hide();
+		shutdown();
 	}
 
 	public void shutdown() {
