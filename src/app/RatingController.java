3c07ee79b84a8c6d7bca2a9db211daa534c5f34b
Aiden Burgess <aiden9874@gmail.com>
2019-10-09 21:02:47 +1300

    Improved rating handling
    Added confirm and cancel button to window

--- src/app/RatingController.java
+++ src/app/RatingController.java
@@ -16,98 +16,149 @@ public class RatingController {
 	@FXML
 	private FontAwesomeIcon star3;
 	@FXML
+	private FontAwesomeIcon star4;
+	@FXML
+	private FontAwesomeIcon star5;
+	@FXML
 	private Label ratingLabel;
 	
-	private Integer click;
+	private Integer rating;
 	private VideoCreation videoCreation;
+	private String selected = "yellow";
+	private String unselected = "black";
 
 	public void setVideo(VideoCreation videoCreation) {
 		this.videoCreation = videoCreation;
 	}
-
 	
 	@FXML
 	private void initialize() {
 		
 	}
 	
+	private void unselectAll() {
+		star1.setStyle("-fx-fill:" + unselected + ";");
+		star2.setStyle("-fx-fill:" + unselected + ";");
+		star3.setStyle("-fx-fill:" + unselected + ";");
+		star4.setStyle("-fx-fill:" + unselected + ";");
+		star5.setStyle("-fx-fill:" + unselected + ";");
+	}
+	
 	@FXML
 	private void star1Enter() {
-        star1.setStyle("-fx-fill:yellow;");
-		star2.setStyle("-fx-fill:black;");
-		star3.setStyle("-fx-fill:black;");
+		unselectAll();
+        star1.setStyle("-fx-fill:" + selected +";");
 	}
 	@FXML
 	private void star2Enter() {
-        star1.setStyle("-fx-fill:yellow;");
-        star2.setStyle("-fx-fill:yellow;");
-		star3.setStyle("-fx-fill:black;");
+		unselectAll();
+        star1.setStyle("-fx-fill:" + selected +";");
+        star2.setStyle("-fx-fill:" + selected +";");
 	}
 	@FXML
 	private void star3Enter() {
-        star1.setStyle("-fx-fill:yellow;");
-        star2.setStyle("-fx-fill:yellow;");
-        star3.setStyle("-fx-fill:yellow;");
+		unselectAll();
+        star1.setStyle("-fx-fill:" + selected +";");
+        star2.setStyle("-fx-fill:" + selected +";");
+        star3.setStyle("-fx-fill:" + selected +";");
+	}
+	@FXML
+	private void star4Enter() {
+		unselectAll();
+        star1.setStyle("-fx-fill:" + selected +";");
+        star2.setStyle("-fx-fill:" + selected +";");
+        star3.setStyle("-fx-fill:" + selected +";");
+        star4.setStyle("-fx-fill:" + selected +";");
+	}
+	@FXML
+	private void star5Enter() {
+		unselectAll();
+        star1.setStyle("-fx-fill:" + selected +";");
+        star2.setStyle("-fx-fill:" + selected +";");
+        star3.setStyle("-fx-fill:" + selected +";");
+        star4.setStyle("-fx-fill:" + selected +";");
+        star5.setStyle("-fx-fill:" + selected +";");
 	}
 	
 	@FXML
 	private void star1Exit() {
-		star1.setStyle("-fx-fill:black;");
-		star2.setStyle("-fx-fill:black;");
-		star3.setStyle("-fx-fill:black;");
-		if (click == null) return;
-		clickPrev();
+		unselectAll();
+		if (rating != null) clickPrev();
 	}
 	@FXML
 	private void star2Exit() {
-		star1.setStyle("-fx-fill:black;");
-		star2.setStyle("-fx-fill:black;");
-		star3.setStyle("-fx-fill:black;");
-		if (click == null) return;
-		clickPrev();
+		unselectAll();
+		if (rating != null) clickPrev();
 	}
 	@FXML
 	private void star3Exit() {
-		star1.setStyle("-fx-fill:black;");
-		star2.setStyle("-fx-fill:black;");
-		star3.setStyle("-fx-fill:black;");
-		if (click == null) return;
-		clickPrev();
+		unselectAll();
+		if (rating != null) clickPrev();
+	}
+	@FXML
+	private void star4Exit() {
+		unselectAll();
+		if (rating != null) clickPrev();
+	}
+	@FXML
+	private void star5Exit() {
+		unselectAll();
+		if (rating != null) clickPrev();
 	}
 	
 	@FXML
 	private void star1Click() {
 		star1Enter();
-        click = 1;
+		rating = 1;
         updateRatingLabel();
 	}
 	@FXML
 	private void star2Click() {
 		star2Enter();
-        click = 2;
+		rating = 2;
         updateRatingLabel();
 	}
 	@FXML
 	private void star3Click() {
 		star3Enter();
-        click = 3;
+		rating = 3;
+        updateRatingLabel();
+	}
+	@FXML
+	private void star4Click() {
+		star4Enter();
+		rating = 4;
+        updateRatingLabel();
+	}
+	@FXML
+	private void star5Click() {
+		star5Enter();
+		rating = 5;
         updateRatingLabel();
 	}
 	
 	private void updateRatingLabel() {
-		ratingLabel.setText("Rating: " + click);
+		ratingLabel.setText("Rating: " + rating);
 	}
-
 	
 	private void clickPrev() {
-		if(click == null) return;
-		if (click.equals(1)) star1Click();
-		if (click.equals(2)) star2Click();
-		if (click.equals(3)) star3Click();
+		if(rating == null) return;
+		if (rating.equals(1)) star1Click();
+		if (rating.equals(2)) star2Click();
+		if (rating.equals(3)) star3Click();
+		if (rating.equals(4)) star4Click();
+		if (rating.equals(5)) star5Click();
 	}
-
-	private void showRating() {
-		System.out.println("Show rating");
+	
+	@FXML
+	private void cancel() {
+		rating = null;
+		confirm();
 	}
-
+	
+	@FXML
+	private void confirm() {
+		System.out.println("User rating is: " + rating);
+		root.getScene().getWindow().hide();
+	}
 }
