b30c3d028b6b7057cbd6ea4f74c07447be76384b
Laura Huijser <54383847+lhuijser@users.noreply.github.com>
2019-10-19 00:00:57 +1300

    Comment and JavaDoc for HoverToolTip

--- src/app/HoverToolTip.java
+++ src/app/HoverToolTip.java
@@ -1,9 +1,14 @@
 package app;
 import javafx.scene.control.Tooltip;
 
+/**
+* Represents a tooltip with text that appears when hovering over a button the tooltip is attached to.
+* Styled according to target user (children ages 7-10)
+*/
 public class HoverToolTip {
     private String desc;
 
+    // Create the tooltip with a given description
     public HoverToolTip(String desc) {
         this.desc=desc;
     }
