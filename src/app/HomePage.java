436a63277a25aa08ba9c45963daeeca525acc2da
Laura Huijser <54383847+lhuijser@users.noreply.github.com>
2019-10-18 23:57:48 +1300

    JavaDoc for the HomePage

--- src/app/HomePage.java
+++ src/app/HomePage.java
@@ -4,7 +4,9 @@ import app.controllers.HomeController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-// Start up the home page
+/**
+* Run the application and start up the home page
+*/
 public class HomePage extends Application {
     @Override
     public void start(Stage stage) throws Exception {
