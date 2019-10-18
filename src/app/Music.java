12c90e917f22144ac2d3da92ad45b0779fa8e9dc
Laura Huijser <54383847+lhuijser@users.noreply.github.com>
2019-10-19 00:05:12 +1300

    Comment and JavaDoc for Music

--- src/app/Music.java
+++ src/app/Music.java
@@ -1,18 +1,24 @@
 package app;
 
+/**
+* Handles access the background music files for the video player
+*/
 public class Music {
 
     private String title;
     private String altTitle;
 
+    // Get the string representing the music
     public Music(String title, String altTitle) {
         this.title=title;
+        this.altTitle=altTitle;
     }
 
     public String getTitle() {
         return this.title;
     }
 
+    // Find the actual name of the music file corresponding to the string input
     public static String findMusic(String name) {
         if (name.equals("Mattioli Prelude")) {
             return "musica";
@@ -25,6 +31,7 @@ public class Music {
         }
     }
 
+    // Represent each music file available with a string
     @Override
     public String toString() {
         if (this.title.equals("musica")) {
