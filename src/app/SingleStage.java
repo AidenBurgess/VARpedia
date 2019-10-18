6767fc6f27d269bc8cd56926ade77a62edda7596
Laura Huijser <54383847+lhuijser@users.noreply.github.com>
2019-10-19 00:06:29 +1300

    Comment SingleStage

--- src/app/SingleStage.java
+++ src/app/SingleStage.java
@@ -8,11 +8,11 @@ public class SingleStage {
 	private static SingleStage singleStage;
 	private Stage stage = new Stage();
 	
+	// Singleton, ensures only one stage is present at a time
 	public static SingleStage getSingleStage() {
 		if (singleStage == null) {
 			singleStage = new SingleStage();
 			singleStage.stage.initStyle(StageStyle.TRANSPARENT);
-//	    	scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
 		}
 		return singleStage;
 	}
