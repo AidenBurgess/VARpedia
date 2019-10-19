038ed72a2837b5af8a061213ab3660536d24ea84
lhuijser <laura.huijser@yahoo.com>
2019-10-19 13:52:17 +1300

    Remove printlns

--- src/app/VideoManager.java
+++ src/app/VideoManager.java
@@ -18,16 +18,13 @@ public class VideoManager {
 	
 	public void add(VideoCreation video) {
 		videos.add(video);
-    	System.out.println(videos);
 	}
 	
 	public void delete(VideoCreation video) {
 		videos.remove(video);
-    	System.out.println(videos);
 	}
 	
     public void writeSerializedVideos() {
-    	System.out.println("Writing serialization");
 		try {
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("VideoCreations.bin"));
 			objectOutputStream.writeObject(videos);
@@ -37,25 +34,19 @@ public class VideoManager {
     }
     
     private ArrayList<VideoCreation> readSerializedVideos() {
-    	System.out.println("Reading serialization");
 		if (videos == null) videos = new ArrayList<>();
 
 		try {
-			ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("VideoCreations.bin"));			
-			System.out.println("Stored in VideoCreations.bin:");
+			ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("VideoCreations.bin"));
 			videos= (ArrayList<VideoCreation>) objectInputStream.readObject();
-			//videos.add((VideoCreation)objectInputStream.readObject());
-			//if (videos == null) videos = new ArrayList<VideoCreation>();
-			System.out.println("Videos now are: " + videos);
 			return videos;
 		} catch (IOException | ClassNotFoundException e) {
-			System.out.println("No videos are saved currently");
+
 		}
 		return videos;
     }
     
     public ArrayList<VideoCreation> getVideos() {
-    	System.out.println("getting videos");
     	if (videos == null) {
     		readSerializedVideos();
     	}
