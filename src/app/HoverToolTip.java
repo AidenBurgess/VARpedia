b0a87f80b1e36425e2bb4262ce86cc346ad857af
lhuijser <laura.huijser@yahoo.com>
2019-10-22 13:35:24 +1300

    Reduced tooltip delay

--- src/app/HoverToolTip.java
+++ src/app/HoverToolTip.java
@@ -1,5 +1,10 @@
 package app;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.control.Tooltip;
+import javafx.util.Duration;
+
+import java.lang.reflect.*;
 
 /**
 * Represents a tooltip with text that appears when hovering over a button the tooltip is attached to.
@@ -27,6 +32,31 @@ public class HoverToolTip {
         tip.setWidth(200.0);
         tip.setWrapText(true);
         tip.setStyle("-fx-font-size: 16");
+        tooltipStartTiming(tip);
         return tip;
+    }
+
+    /**
+     * Shorten the delay time before the tooltip message appears on screen through Reflection
+     */
+    public static void tooltipStartTiming(Tooltip tip) {
+        try {
+            // Access the tooltip's behaviour field and save it's contents to a variable
+            Field fieldBehaviour = tip.getClass().getDeclaredField("BEHAVIOR");
+            fieldBehaviour.setAccessible(true);
+            Object objBehaviour = fieldBehaviour.get(tip);
+
+            // Access the tooltip's timer field and save it's contents to a variable
+            Field fieldTimer = objBehaviour.getClass().getDeclaredField("activationTimer");
+            fieldTimer.setAccessible(true);
+            Timeline objTimer = (Timeline) fieldTimer.get(objBehaviour);
+
+            // Set the new delay time for the tooltip message appearance
+            objTimer.getKeyFrames().clear();
+            objTimer.getKeyFrames().add(new KeyFrame(new Duration(50)));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
