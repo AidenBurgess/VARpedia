e5dde1c7b714117279ea48c63719e42605d69c77
Aiden Burgess <aiden9874@gmail.com>
2019-10-23 17:43:55 +1300

    Cleared old favourite code

--- src/app/controllers/ReviewController.java
+++ src/app/controllers/ReviewController.java
@@ -76,13 +76,6 @@ public class ReviewController extends DraggableWindow {
 	private VideoCreation currentVideo;
 	private int playIndex = 0;
 
-	// Set style for favourite icon
-//	private final String selected = "-fx-fill:rgb(233.0,195.0,248.0);";
-//	private final String unselected = "-fx-fill:black;";
-
-	// Keep track of whether the video is favourited or not
-	private boolean favourite;
-
 	/***************************** FXML METHODS ********************************/
 
 	/**
@@ -271,10 +264,7 @@ public class ReviewController extends DraggableWindow {
 	 * Checks the current status of the favourite aspect of the currently playing video, and set up the star icon as such
 	 */
 	private void setUpStar() {
-		favourite = currentVideo.getFavourite();
-		System.out.println(favourite);
-		
-		if (favourite) {
+		if (currentVideo.getFavourite()) {
 			favHeart.setIcon(MaterialDesignIcon.HEART);
 		} else {
 			favHeart.setIcon(MaterialDesignIcon.HEART_OUTLINE);
@@ -472,10 +462,6 @@ public class ReviewController extends DraggableWindow {
 	public void shutdown() {
 		player.dispose();
 		music.dispose();
-	}
-
-	public Boolean getFavourite() {
-		return favourite;
 	}
 
 }
